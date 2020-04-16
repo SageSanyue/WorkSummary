dd19fa83e8a274ae5908e498e5ddbd8611dd9867
SageSanyue <562441461@qq.com>
2020-04-16 22:36:57 +0800

    Vue版本落地页

---
+++ loadingPageDemo/data/data.js
@@ -0,0 +1,60 @@
+var mockData =
+{
+    "status": {
+        "code": "0",
+        "description": "success [信息码:e603da]"
+    },
+    "result": {
+        "total": 4,
+        "list": [
+            {
+                "appId": "WX01",
+                "name": "找合作找合作找合作找合作找合作",
+                "slogan": "1个交流群交流群交流群交流群交流群",
+                "desc": "交流群交流群流群!交流群交流群",
+                "jumpUrl": "null",
+                "type": 2,
+                "isBeta": 0,
+                "bannerPicUrl": "https://sm.ms/image/rmDMthvzRwefUuc",
+                "qrCodePicUrl": "https://i.loli.net/2020/04/16/zXA4gVEe75hKl1Z.jpg",
+                "logoPicUrl": "https://i.loli.net/2020/04/16/rmDMthvzRwefUuc.png"
+            },
+            {
+                "appId": "PC01",
+                "name": "登月计划",
+                "slogan": "连接宇宙",
+                "desc": "我们的征途是星辰大海我们的征途是星辰大海我们的征途是星辰大海",
+                "jumpUrl": "http://zuhou.huizhaofang.com",
+                "type": 1,
+                "isBeta": 0,
+                "bannerPicUrl": "https://i.loli.net/2020/04/16/Bp8TErW3w5LDsO9.png",
+                "qrCodePicUrl": null,
+                "logoPicUrl": "https://i.loli.net/2020/04/16/Bp8TErW3w5LDsO9.png"
+            },
+            {
+                "appId": "WX01",
+                "name": "发射器",
+                "slogan": "今天也要加油呀",
+                "desc": "我今天哪儿也不去什么都不干就学习我今天哪儿也不去什么都不干就学习我今天哪儿也不去什么都不干就学习过程，我今天哪儿也不去什么都不干就学习。",
+                "jumpUrl": "http://huihujiao.huizhaofang.com/",
+                "type": 2,
+                "isBeta": 1,
+                "bannerPicUrl": "https://i.loli.net/2020/04/16/ScnPWXG5IbQvxyK.png",
+                "qrCodePicUrl": "https://i.loli.net/2020/04/16/zXA4gVEe75hKl1Z.jpg",
+                "logoPicUrl": "https://i.loli.net/2020/04/16/ScnPWXG5IbQvxyK.png"
+            },
+            {
+                "appId": "PC523e48f7b9f34c20a4160ce3c8a295c1",
+                "name": "机车",
+                "slogan": "单车变摩托摩托变汽车",
+                "desc": "“爱学习”我今天哪儿也不去什么都不干就学习。",
+                "jumpUrl": "https://i.loli.net/2020/04/16/PQGwKIi9hx56Mej.png",
+                "type": 1,
+                "isBeta": 1,
+                "bannerPicUrl": "https://i.loli.net/2020/04/16/PQGwKIi9hx56Mej.png",
+                "qrCodePicUrl": null,
+                "logoPicUrl": "https://i.loli.net/2020/04/16/PQGwKIi9hx56Mej.png"
+            }
+        ]
+    }
+}
---
+++ loadingPageDemo/distVue/js/index.js
@@ -0,0 +1,84 @@
+// 页面初始化操作
+ var vm = new Vue({
+    el: '#index',
+    data: {
+       list: []
+    },
+    
+    mounted: function () {
+        var res = mockData.result.list
+        this.list = res
+        // var url = 'http://www.sage.com';
+        // ajax({
+        //     url: url,
+        //     type: "POST",
+        //     data: '',
+        //     dataType: "json",
+        //     success: function (res) {
+        //         var res = JSON.parse(response)
+        //         this.list = res.result.list
+        //     },
+        //     fail: function (err) {
+        //         console.log(err, 'err')
+        //     }
+        // })
+    },
+    methods: {
+        addActive(index, type) {
+            if (type == 2) {
+                console.log(111)
+                $(`.main-item.inner${index}`).addClass('active');
+            }
+        },
+        removeActive(index, type) {
+            if (type == 2) {
+                $('.main-item').removeClass('active');
+            }
+        }
+       
+    }
+});
+
+function ajax(options) {
+   options = options || {};
+   options.type = (options.type || "GET").toUpperCase();
+   options.dataType = options.dataType || "json";
+   var params = formatParams(options.data);
+   //创建 - 非IE6 - 第一步
+   if (window.XMLHttpRequest) {
+       var xhr = new XMLHttpRequest();
+   } else {
+       //IE6及其以下版本浏览器
+       var xhr = new ActiveXObject('Microsoft.XMLHTTP');
+   }
+   //接收 - 第三步
+   xhr.onreadystatechange = function () {
+       if (xhr.readyState == 4) {
+           var status = xhr.status;
+           if (status >= 200 && status < 300) {
+               options.success && options.success(xhr.responseText, xhr.responseXML);
+           } else {
+               options.fail && options.fail(status);
+           }
+       }
+   }
+   //连接 和 发送 - 第二步
+   if (options.type == "GET") {
+       xhr.open("GET", options.url + "?" + params, true);
+       xhr.send(null);
+   } else if (options.type == "POST") {
+       xhr.open("POST", options.url, true);
+       //设置表单提交时的内容类型
+       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+       xhr.send(params);
+   }
+}
+//格式化参数
+function formatParams(data) {
+    var arr = [];
+    for (var name in data) {
+        arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
+    }
+    arr.push(("v=" + Math.random()).replace(".",""));
+    return arr.join("&");
+ }
